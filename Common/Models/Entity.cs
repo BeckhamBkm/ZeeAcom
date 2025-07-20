01402e9aed605a63a20875fd1f040786980c455f
BeckhamBkm <bokang.modisebkm@gmail.com>
2025-07-20 16:26:12 +0200

    Created new models for an entity and owner.

---
+++ Common/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace ZeeAcom.Common.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; }= string.Empty;
+        public string Suburb { get; set; }= string.Empty;
+        public bool Active { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Guid OwnerId { get; set; }
+        public Guid SessionId { get; set; }
+    }
+}
---
+++ Common/Models/Owner.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace ZeeAcom.Common.Models
+{
+    public class Owner
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }= string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string CellPhone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
